9e034cd862c454859ef1559624041ce3c8e4f491
Freskinaa <sfreskina@gmail.com>
2024-10-23 22:59:13 +0200

    added pagination in products page

--- products.js
+++ products.js
@@ -1,60 +1,101 @@
-let currentStartIndex = 0;
-document.getElementById("viewMoreBtn").addEventListener("click", function () {
-  const allCards = document.querySelectorAll(".coffeetypes__card");
+// let currentStartIndex = 0;
+// document.getElementById("viewMoreBtn").addEventListener("click", function () {
+//   const allCards = document.querySelectorAll(".coffeetypes__card");
 
-  for (
-    let i = currentStartIndex;
-    i < currentStartIndex + 6 && i < allCards.length;
-    i++
-  ) {
-    allCards[i].classList.add("hidden");
-    allCards[i].classList.remove("add");
+//   for (
+//     let i = currentStartIndex;
+//     i < currentStartIndex + 6 && i < allCards.length;
+//     i++
+//   ) {
+//     allCards[i].classList.add("hidden");
+//     allCards[i].classList.remove("add");
+//   }
+
+//   currentStartIndex += 6;
+
+//   for (
+//     let i = currentStartIndex;
+//     i < currentStartIndex + 6 && i < allCards.length;
+//     i++
+//   ) {
+//     allCards[i].classList.remove("hidden");
+//     allCards[i].classList.add("add");
+//   }
+
+//   if (currentStartIndex + 6 >= allCards.length) {
+//     this.style.display = "none";
+//   }
+
+//   document.getElementById("backBtn").style.display = "inline-block";
+// });
+
+// document.getElementById("backBtn").addEventListener("click", function () {
+//   const allCards = document.querySelectorAll(".coffeetypes__card");
+
+//   for (let i = currentStartIndex - 6; i < currentStartIndex && i >= 0; i++) {
+//     allCards[i].classList.remove("hidden");
+//     allCards[i].classList.add("add");
+//   }
+
+//   for (
+//     let i = currentStartIndex;
+//     i < currentStartIndex + 6 && i < allCards.length;
+//     i++
+//   ) {
+//     allCards[i].classList.add("hidden");
+//     allCards[i].classList.remove("add");
+//   }
+
+//   currentStartIndex -= 6;
+
+//   if (currentStartIndex <= 0) {
+//     currentStartIndex = 0;
+//     this.style.display = "none";
+//   }
+
+//   document.getElementById("viewMoreBtn").style.display = "inline-block";
+// });
+let currentPage = 1;
+const itemsPerPage = 6;
+
+function showPage(page) {
+  const allCards = document.querySelectorAll(".coffeetypes__card");
+  const totalItems = allCards.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  if (page < 1 || page > totalPages) {
+    return;
   }
 
-  currentStartIndex += 6;
+  currentPage = page;
 
-  for (
-    let i = currentStartIndex;
-    i < currentStartIndex + 6 && i < allCards.length;
-    i++
-  ) {
+  allCards.forEach((card) => card.classList.add("hidden"));
+
+
+  const start = (page - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  for (let i = start; i < end && i < totalItems; i++) {
     allCards[i].classList.remove("hidden");
     allCards[i].classList.add("add");
   }
 
-  if (currentStartIndex + 6 >= allCards.length) {
-    this.style.display = "none";
-  }
 
-  document.getElementById("backBtn").style.display = "inline-block";
+  document.querySelectorAll(".page-number").forEach((number) => {
+    number.classList.remove("active");
+  });
+  document.querySelector(`.page-number[data-page="${page}"]`).classList.add("active");
+}
+
+
+document.querySelectorAll(".page-number").forEach((number) => {
+  number.addEventListener("click", () => {
+    const page = parseInt(number.dataset.page);
+    showPage(page);
+  });
 });
 
-document.getElementById("backBtn").addEventListener("click", function () {
-  const allCards = document.querySelectorAll(".coffeetypes__card");
+showPage(1);
 
-  for (let i = currentStartIndex - 6; i < currentStartIndex && i >= 0; i++) {
-    allCards[i].classList.remove("hidden");
-    allCards[i].classList.add("add");
-  }
-
-  for (
-    let i = currentStartIndex;
-    i < currentStartIndex + 6 && i < allCards.length;
-    i++
-  ) {
-    allCards[i].classList.add("hidden");
-    allCards[i].classList.remove("add");
-  }
-
-  currentStartIndex -= 6;
-
-  if (currentStartIndex <= 0) {
-    currentStartIndex = 0;
-    this.style.display = "none";
-  }
-
-  document.getElementById("viewMoreBtn").style.display = "inline-block";
-});
 
 const productNames = [
   "Americano",
