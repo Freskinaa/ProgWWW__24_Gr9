20c3f55b923ebdce15ca385bfbef5adf013760a5
eronaaaaaaaaaa <eronagojnovcii@gmail.com>
2024-10-27 23:15:27 +0100

    fixed pricing on the products page and fixed functionality

--- order.js
+++ order.js
@@ -1,22 +1,42 @@
 const coffeePrices = {
-  espresso: 2.00,
-  latte: 3.00,
-  cappuccino: 2.50
+  espresso: 3.00,
+  latte: 4.00,
+  cappuccino: 3.50,
+  americano: 5.00,
+  IcedMocha: 4.00,
+  frappe: 2.50,
+  coldBrew: 3.00,
+  breve: 2.75,
+  TurkishCoffee: 2.50,
+  cortado: 3.50,
+  dalgona: 3.50,
+  dirtychai: 4.50,
+  mazagran: 2.80,
+  flatWhite: 2.50,
+  dopio: 1.50,
+  arabica: 2.50,
+  galao: 2.00,
+  mocha: 4.5,
+  macchiato: 3.75,
+  affogato: 3.25,
+  nitroColdBrew: 4.00,
+  IrishCoffee: 2.75,
+  decaf: 3.00
 };
 
 function animateTotalCost(target, start, end, duration) {
   let startTime = null;
 
   function step(timestamp) {
-      if (!startTime) startTime = timestamp;
-      const progress = Math.min((timestamp - startTime) / duration, 1);
-      target.innerText = `$${(start + progress * (end - start)).toFixed(2)}`;
-      if (progress < 1) {
-          window.requestAnimationFrame(step);
-      } else {
-          target.classList.add("updated");
-          setTimeout(() => target.classList.remove("updated"), 300);
-      }
+    if (!startTime) startTime = timestamp;
+    const progress = Math.min((timestamp - startTime) / duration, 1);
+    target.innerText = `$${(start + progress * (end - start)).toFixed(2)}`;
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    } else {
+      target.classList.add("updated");
+      setTimeout(() => target.classList.remove("updated"), 300);
+    }
   }
 
   window.requestAnimationFrame(step);
