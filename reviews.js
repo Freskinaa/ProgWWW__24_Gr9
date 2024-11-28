0768f442b78af8d9bcf3d1cc8caed7ec1a31013b
Erona <eronagojnovcii@gmail.com>
2024-11-28 14:08:33 +0100

    fixed reviews

--- reviews.js
+++ reviews.js
@@ -1,65 +1,19 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let currentIndex = 0;
 
-function plusSlides(n) {
-    showSlides((slideIndex += n));
-}
+function plusSlides(direction) {
+  const reviewsGrid = document.querySelector('.reviews-grid');
+  const cards = document.querySelectorAll('.cards');
+  const cardWidth = cards[0].offsetWidth + 20; 
+  const totalCards = cards.length;
+  const visibleCards = 3;
 
-function currentSlide(n) {
-    showSlides((slideIndex = n));
-}
+  currentIndex += direction;
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+  if (currentIndex < 0) {
+    currentIndex = totalCards - visibleCards;
+  } else if (currentIndex > totalCards - visibleCards) {
+    currentIndex = 0;
+  }
 
-
-
-
-
-let slideIndex2 = 1;
-showSlides2(slideIndex2);
-
-function plusSlides2(n) {
-    showSlides2((slideIndex2 += n));
-}
-
-function currentSlide2(n) {
-    showSlides2((slideIndex2 = n));
-}
-
-function showSlides2(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides2");
-    let dots = document.getElementsByClassName("dot2");
-    if (n > slides.length) {
-        slideIndex2 = 1;
-    }
-    if (n < 1) {
-        slideIndex2 = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex2 - 1].style.display = "block";
-    dots[slideIndex2 - 1].className += " active";
+  reviewsGrid.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
 }
