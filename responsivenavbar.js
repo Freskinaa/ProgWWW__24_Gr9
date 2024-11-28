0f4420e994e0a8c72639e514b6edce6b3d88696c
Erona <eronagojnovcii@gmail.com>
2024-11-28 23:56:10 +0100

    fixed navbar

--- responsivenavbar.js
+++ responsivenavbar.js
@@ -1,6 +1,17 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navLinks = document.querySelector('.nav__links');
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navLinks = document.querySelector('.nav__links');
+  const accountLink = document.getElementById("accountLink");
 
-hamburgerMenu.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+  hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+
+  const isLoggedIn = true;
+
+  if (isLoggedIn) {
+    accountLink.style.display = "block";
+  } else {
+    accountLink.style.display = "none";
+  }
 });
